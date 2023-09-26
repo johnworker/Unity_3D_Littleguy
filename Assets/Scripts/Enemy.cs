ea61fe47c04187e21c54c22dddec522e3abdb3b8
johnworker <78348941+johnworker@users.noreply.github.com>
2023-09-26 20:36:07 +0800

    追蹤設定

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField,Header("移動速度"), Range(0, 10)]
+    private float speed = 3f;
+
+    private NavMeshAgent agent;
+
+    private Transform target;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.speed = speed;
+
+        target = GameObject.Find("小黑").transform;
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
